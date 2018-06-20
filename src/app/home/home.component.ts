6fff14d5f87f0598cacc9417508c5a3fa2eeeabd
ashwiniagre1 <ashwiniagre1>
2018-06-20 04:33:55 +0000

    Initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,9 @@ import {Router} from "@angular/router";
   templateUrl: 'home.html'
 })
 export class HomeComponent {
-   menuData:any;	
   constructor(private _router : Router){
-    
- this.menuData=[{"text":"Core","expand":true,"children":[{"text":"TestUi","routerLink":"core/testui","selected":false},{"text":"Employee Details","routerLink":"core/employeedetails","selected":false},{"text":"Employee List","routerLink":"core/employeelist","selected":false}]}];   }
+
+  }
   routeToLink(data: any){
      if(!data.hasOwnProperty('children') && data.routerLink)
       this._router.navigate([data.routerLink]);
